2a135206bb5f03f04bd5e90549cc4fff1f8ca969
ashfimzk <ashfimzk@gmail.com>
2023-01-23 10:54:13 +0700

    create commit

--- gitcommands.js
+++ gitcommands.js
@@ -1,4 +1,6 @@
-// 1 git init 
-// 2 git add index.js/git add
+// 1 git init (inside project directory )(warning first initiate only)
+ // 2 git add index.js/git add
 // 3 git commit -m "create variable inside index.js"
 // 4 git log
+// 5 git remote add origin git remote add origin https://github.com/ashfimzk/JCWD2302-SCM.git (warning:first initiate only)
+// 6 git push origin master 
